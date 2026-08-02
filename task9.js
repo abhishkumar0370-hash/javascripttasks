957e033472275a1088f7af47e5ea5afdbd1870b7
abhishkumar0370@gmail.com <abhishkumar0370@gmail.com>
2026-08-03 01:00:20 +0500

    javascript task9

---
+++ task9.js
@@ -0,0 +1,13 @@
+// print table with even/odd result
+let i=1;
+
+while (i<=10) {
+    
+let result= 2 * i;
+if(result % 2 ===0){
+    console.log(result + " even");
+} else{
+    console.log(result + " odd")
+}
+i++;
+}
