ac835a9fa6401d580a8f26311790a5769d644058
abhishkumar0370@gmail.com <abhishkumar0370@gmail.com>
2026-08-02 12:56:47 +0500

    javascript task

---
+++ task2.js
@@ -0,0 +1,12 @@
+let i=-5;
+
+while (i<=10) {
+    if(i > 0){
+        console.log(i + " Positve Number");
+    } else if(i < 0){
+        console.log(i + " negative number");
+    } else{
+        console.log(i + " Zero");
+    }
+    i++;
+}
