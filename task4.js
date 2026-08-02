5bc700040c61c7824bc3f76955e5440c84ed9e08
abhishkumar0370@gmail.com <abhishkumar0370@gmail.com>
2026-08-02 13:28:09 +0500

    javascript task4

---
+++ task4.js
@@ -0,0 +1,12 @@
+// numbers greater than 5
+
+let i=1;
+
+while (i<=10) {
+    if (i > 5){
+        console.log(i + " greater than 5");
+    }else{
+        console.log(i + " less than 5");
+    }
+    i++;
+}
